626e242ebac580462346d6deb9172b5342ddad1d
Yeshinegus <toynigus@gmail.com>
2021-06-21 08:32:05 -0700

    5th file snipet update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
+ * main - entry point
  *
- *main - entry point
- *
- *Return: always 0
+ * Return: always 0 (success)
  *
  */
 int main(void)
 {
-    printf("with proper grammer, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammer, but the outcome is a piece of art,\n");
+return (0);
 }
