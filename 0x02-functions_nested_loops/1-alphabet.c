612de318b0cd2d082869d4f2f5285d3225eb5e36
Yeshinegus <toynigus@gmail.com>
2021-06-22 13:51:45 -0700

    first 0x02 commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int main(void)
+{
+_putchar(print_alphabet());
+
+}
+
+void print_alphabet()
+{
+char i;
+ for (i = 'a'; i <= 'z'; i++)
+   _putchar(i);
+}
