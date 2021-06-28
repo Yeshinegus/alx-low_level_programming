29204912327f3c14ec1710a34fc43c5bed2f24a3
Yeshinegus <toynigus@gmail.com>
2021-06-28 12:35:06 -0700

    correction on print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 /**
  * print_rev - returns a string length
  *
- * @s: is an integer pointer 
+ * @s: is an integer pointer
  *
  */
 void print_rev(char *s)
@@ -12,8 +12,8 @@ int i, j;
 j = 0;
 for (i = 0; s[i] != '\0'; i++)
 j++;
- 
-for (i = j-1; i >= j; i--)
+
+for (i = j - 1; i >= j; i--)
 _putchar(s[i]);
 _putchar('\n');
 
