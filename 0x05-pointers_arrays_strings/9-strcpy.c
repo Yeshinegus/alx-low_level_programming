5e40fc38af73c645c4bc39d1d688e6acc17c3350
Yeshinegus <toynigus@gmail.com>
2021-06-29 11:04:06 -0700

    update on strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,16 +1,17 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * _strcpy - copy of an array
+ * *_strcpy - copy of an array
  *
  * @dst: is a char array
  * @src: is a char array
+ * Return: type char array
  */
 char *_strcpy(char *dst, char *src)
 {
-  int i;
-  for(i = 0; src[i] != '\0'; i++)
-    dst[i] = src[i];
-  dst[i + 1] = '\0';
-  return (dst);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+dst[i] = src[i];
+dst[i + 1] = '\0';
+return (dst);
 }
