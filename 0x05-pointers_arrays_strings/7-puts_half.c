1e90ef3f57365652a8bc523d4b11373a4de3a9c4
Yeshinegus <toynigus@gmail.com>
2021-06-29 13:43:28 -0700

    updated puts-half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,8 +10,10 @@ void puts_half(char *s)
 
 int i, half;
 half = 0;
-for (i = 0; s[i] != '\0'; i++)
+while (s[half])
+{
 half++;
+}
 for (i = half / 2; s[i] != '\0'; i++)
 _putchar(s[i]);
 _putchar('\n');
