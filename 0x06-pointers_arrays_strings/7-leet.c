1fd3e28cc9ca9ae5c9d235d180704c0b896f7f3e
Yeshinegus <toynigus@gmail.com>
2021-07-02 09:56:45 -0700

    update still 7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,8 +8,7 @@
 char *leet(char *c)
 {
 int i;
-i = 0;
-while (*(c + i) != '\0')
+ for (i = 0; *(c + i) != '\0'; i++)
 {
 if (c[i] == 'a' || c[i] == 'A')
 c[i] = '4';
@@ -21,7 +20,7 @@ else if (c[i] == 't' || c[i] == 'T')
 c[i] = '7';
 else if (c[i] == 'l' || c[i] == 'L')
 c[i] = '1';
-i++;
+
 }
 return (c);
 }
