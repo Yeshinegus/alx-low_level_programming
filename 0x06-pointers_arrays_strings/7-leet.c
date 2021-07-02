c38e92f76c554a17f00a2cc332dfa0772e0a25dd
Yeshinegus <toynigus@gmail.com>
2021-07-02 09:36:53 -0700

    updated 7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 /**
- * leet - function of 
+ * leet - function of
  * @c: pointer variable
  *
  * Return: char var
  */
 char *leet(char *c)
 {
-  int i;
-  i = 0;
-while (c[i] != '\0')
+int i;
+i = 0;
+while (*(c + i) != '\0')
 {
 if (c[i] == 'a' || c[i] == 'A')
 c[i] = '4';
-else if (c[i] == 'e' || c[i] == 'C')
+else if (c[i] == 'e' || c[i] == 'E')
 c[i] = '3';
 else if (c[i] == 'o' || c[i] == 'O')
 c[i] = '0';
@@ -21,6 +21,7 @@ else if (c[i] == 't' || c[i] == 'T')
 c[i] = '7';
 else if (c[i] == 'l' || c[i] == 'L')
 c[i] = '1';
+i++;
 }
 return (c);
 }
