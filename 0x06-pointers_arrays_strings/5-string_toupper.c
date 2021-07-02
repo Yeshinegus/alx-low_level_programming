7c427309fb6142ed95d2f42b4e9e88ca7e235335
Yeshinegus <toynigus@gmail.com>
2021-07-01 17:15:48 -0700

    correction on toupper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,7 +6,7 @@
  * Return: nothing to return
  *
  */
-void string_toupper(char *a)
+char *string_toupper(char *a)
 {
 int i;
 i = 0;
@@ -14,13 +14,9 @@ i = 0;
 while (a[i])
 {
 if (a[i] >= 'a' && a[i] <= 'z')
-{
 a[i] = a[i] - 32;
-_putchar(a[i]);
-}
-else
-_putchar(a[i]);
+
 i++;
 }
-_putchar('\n');
+return (a);
 }
