efc49d08e5c493e942e5b54d681d653b112ed9f4
Yeshinegus <toynigus@gmail.com>
2021-07-05 14:00:01 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,26 +9,25 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-  unsigned int i, j, index, checker;
-  checker = 0;
-  index = 0;
+unsigned int i, j, index, checker;
+checker = 0;
+index = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-  checker = 0;
-  for (j = 0; accept[j] != '\0'; j++)
-  {
-    if (s[i] == accept[j])
-      {
-    checker = 1;
-    if (i > index)
-      index = i;
-       }
-  }
-  if (checker == 0)
-      return (0);
+checker = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+checker = 1;
+if (i > index)
+index = i;
 }
-  while (s[index] != ' ' || s[index] != '\0')
-    index++;
-
+}
+if (checker == 0)
+return (0);
+}
+while (s[index] != ' ' || s[index] != '\0')
+index++;
 return (index);
 }
