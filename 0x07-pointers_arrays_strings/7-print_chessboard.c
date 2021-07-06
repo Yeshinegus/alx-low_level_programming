32546e18856749c602d731347996d35215e8a8cf
Yeshinegus <toynigus@gmail.com>
2021-07-06 10:43:51 -0700

    print chess

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,12 +9,13 @@
 
 void print_chessboard(char (*a)[8])
 {
-unsigned int i, j;
-for (i = 0; ; i++)
+unsigned int i, j = 8;
+for (i = 0; i < 8; i++)
 {
-for (j = 0; a[i][j] != '\0'; j++)
+for (j = 0; i < 8; j++)
+{
 _putchar(a[i][j]);
-
+}
 _putchar('\n');
 }
 }
