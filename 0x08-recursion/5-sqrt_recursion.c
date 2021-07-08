5c789393db5b8fa2d80d20183d7b7de77e47ddd7
Yeshinegus <toynigus@gmail.com>
2021-07-08 13:48:05 -0700

    sqrt update

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - function of str concatnate
+ *
+ * @x: an int
+ *
+ * Return: nothing
+ *
+ */
+int _sqrt_recursion(int x)
+{
+int y;
+y = -1;
+if (x == 0 || x == 1)
+return (x);
+
+if (x >= 4)
+{
+
+y = _sqrt_recursion(x-1);
+
+if (y * y == x)
+return (y);
+
+}
+
+return (-1);
+}
