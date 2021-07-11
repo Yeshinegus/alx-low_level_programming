5065d4395a6388187494ceb4da64be5c3246ec7b
Yeshinegus <toynigus@gmail.com>
2021-07-11 09:12:39 -0700

    update on print rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,16 +8,11 @@
  */
 void _print_rev_recursion(char *s)
 {
-int i, j;
-i = j = 0;
-if (s[i] != '\0')
-j++;
-else if (s[i] == '\0' && j > 0)
+if (*s)
 {
-_putchar(s[j - 1]);
-j--;
-_print_rev_recursion((s + (j - 1)));
+_print_rev_recursion(s + 1);
+_putchar(*s);
 
 }
-_putchar('\n');
+
 }
