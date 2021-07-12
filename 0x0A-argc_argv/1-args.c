f93448d2335b4dec5ed61b943fee83e3bf357d43
Yeshinegus <toynigus@gmail.com>
2021-07-12 13:58:30 -0700

    2nd commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - code entry poing
+ *
+ * @argc: int parameter
+ *
+ * @argv: pointer argument
+ */
+int main (int argc, char *argv[])
+{
+	if (argv)
+	printf("%d\n", argc);
+	return (0);
+}
