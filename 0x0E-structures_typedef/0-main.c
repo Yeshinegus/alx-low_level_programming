16f3f4a640ac99c156b82a3c21f7e54dcacc6d21
Yeshinegus <toynigus@gmail.com>
2021-07-20 14:49:12 -0700

    all commit

--- 0x0E-structures_typedef/0-main.c
+++ 0x0E-structures_typedef/0-main.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include "dog.h"
+#include "dog.h"
 
 /**
- *
  * main - check the code for ALX Shool students
  *
- * Return: always 0 (success)
+ * Return: int always 0 (success)
  *
  */
 
 int main(void)
 {
 	struct dog my_dog;
-	
+
 	my_dog.name = "Poppy";
 	my_dog.age = 3.5;
 	my_dog.owner = "Bob";
-	printf("My name is %s and I am %.1f :) - Woof\n", my_dog.name, my_dog.age );
+	printf("My name is %s and I am %.1f :) - Woof\n", my_dog.name, my_dog.age);
 	return (0);
 
 }
