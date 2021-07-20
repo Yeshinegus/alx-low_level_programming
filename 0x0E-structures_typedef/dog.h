beeb175f82283a369d16c7cbcd79196329695fd0
Yeshinegus <toynigus@gmail.com>
2021-07-20 13:41:51 -0700

    update on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,30 @@
-
-struct dog {
-	char * name;
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct
+ * @name: name of dog
+ * @age: age of doge
+ * @owner: dog's owner
+ *
+ */
+struct dog
+{
+	char *name;
 	float age;
 	char *owner;
-}dog_t;
-
-struct dog_t {
-	char * name;
+};
+/**
+ * struct dog_t - structure
+ * @name: name of the dog
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ */
+struct dog_t
+{
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
 
-
+#endif
