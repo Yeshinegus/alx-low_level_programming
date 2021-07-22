a3d13a8c9c7489bc7a8fea0476a656518cc78a09
Yeshinegus <toynigus@gmail.com>
2021-07-21 18:50:59 -0700

    betty update on int index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
-/*
+/**
  * int_index - prints the array index
  * @array: integer array
  * @size: size of the array
@@ -8,14 +8,14 @@
  *
  * Return: integer
  */
-int int_index (int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if(size <= 0)
+if (size <= 0)
 	return (-1);
 for (i = 0; i < size; i++)
-if(cmp(array[i]))
+if (cmp(array[i]))
 	return (i);
 
 return (-1);
