5315e0c1c6089ff1e9488e5e8859e397418c95b5
Yeshinegus <toynigus@gmail.com>
2021-08-04 14:18:35 -0700

    get node int commit

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * get_nodeint_at_index - get nth node
+ * @head: heade pointer
+ * @index: index of the node
+ * 
+ * Return: the node
+ *
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *h;
+unsigned int i;
+
+h = head;
+if (head == NULL)
+{
+	return (NULL);
+}
+for(i = 0; h != NULL && i != index; i++)
+{
+h = h->next;
+if (i == index)
+	return (h);
+}
+return (NULL);
+}
